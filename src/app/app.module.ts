83392f12a66b8fcadefa61a19076d64a36516fff
Laura Mucaraku <lmucaraku19@epoka.edu.al>
2022-04-22 18:14:43 +0200

    Ndryshova service-page. Tani ruhet cdo sherbim i selektuar dhe llogaritet edhe koha totale e cdo appointment-i.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-//
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { ServicesPageComponent } from './services-page/services-page.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatIconModule } from "@angular/material/icon";
-// import { MatButtonModule } from "@angular/material/button";
-//
-// // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-//
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     ServicesPageComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     NgbModule,
-//     BrowserAnimationsModule,
-//     MatButtonModule,
-//     MatIconModule
-//     // NgbModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
