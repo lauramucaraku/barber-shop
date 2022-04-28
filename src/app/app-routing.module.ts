288d7eaab4abdc6a51ffbf445fb76b7a5a0cfc17
ElinorPrenja <elinorprenja17@gmail.com>
2022-04-28 04:18:15 +0200

    router i service component ne app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import {NavbarComponent} from "./navbar/navbar.component";
-// import {ServicesPageComponent} from "./services-page/services-page.component";
+import {ServicesPageComponent} from "./services-page/services-page.component";
 
 const routes: Routes = [
-  // {path:"home", component: NavbarComponent},
-  // {path:"services", component:ServicesPageComponent}
-  // {path:"location", component:NavbarComponent},
-  // {path:"aboutus", component:NavbarComponent},
-  // {path:"login", component:NavbarComponent}
+  {path:"services", component:ServicesPageComponent}
+
 ];
 
 @NgModule({
