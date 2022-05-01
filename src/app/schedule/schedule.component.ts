da88901d9e69c93258ba76923ceae29e608493f0
ElinorPrenja <elinorprenja17@gmail.com>
2022-05-01 03:49:18 +0200

    Shtojme evente te gatshme

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -4,90 +4,78 @@ import { MbscEventcalendarOptions, Notifications, MbscCalendarEvent,MbscDatepick
   MbscPopupOptions,setOptions } from '@mobiscroll/angular';
 import { HttpClient } from '@angular/common/http';
 
+setOptions({
+  theme: 'material',
+  themeVariant: 'light'
+});
+
+
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.scss']
+  styleUrls: ['./schedule.component.scss'] ,
+  providers: [Notifications]
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private notify: Notifications) { }
 
-  ngOnInit(): void {
-  }
 
-}
-//
-// // setOptions({
-// //   theme: 'material',
-// //   themeVariant: 'light'
-// // });
-//
-// @Component({
-//   selector: 'app-settings',
-//   templateUrl: './settings.component.html',
-//   styleUrls: ['./settings.component.scss'],
-//   // providers: [Notifications]
-// })
-// export class SettingsComponent implements OnInit {
-//
-//   constructor(/*private http: HttpClient, private notify: Notifications*/) { }
-//
-//   // @ViewChild('popup', { static: false })
-//   // popup!: MbscPopup;
-//   // @ViewChild('colorPicker', { static: false })
-//   // colorPicker: any;
-//   // popupEventTitle: string | undefined;
-//   // popupEventDescription = '';
-//   // popupEventAllDay = true;
-//   // popupEventDates: any;
-//   // popupEventStatus = 'busy';
-//   // calendarSelectedDate: any = new Date();
-//   // switchLabel: any = 'All-day';
-//   // tempColor = '';
-//   // selectedColor = '';
-//   // colorAnchor: HTMLElement | undefined;
-//   // colors = ['#ffeb3c', '#ff9900', '#f44437', '#ea1e63', '#9c26b0', '#3f51b5', '', '#009788', '#4baf4f', '#7e5d4e'];
-//   //
-//   //
-//   // myEvents: MbscCalendarEvent[] = [{
-//   //   id: 1,
-//   //   start: '2022-04-08T13:00',
-//   //   end: '2022-04-08T13:45',
-//   //   title: 'Lunch @ Butcher\'s',
-//   //   description: '',
-//   //   allDay: false,
-//   //   free: true,
-//   //   color: '#009788'
-//   // }, {
-//   //   id: 2,
-//   //   start: '2022-04-30T15:00',
-//   //   end: '2022-04-30T16:00',
-//   //   title: 'General orientation',
-//   //   description: '',
-//   //   allDay: false,
-//   //   free: false,
-//   //   color: '#ff9900'
-//   // }, {
-//   //   id: 3,
-//   //   start: '2022-04-29T18:00',
-//   //   end: '2022-04-29T22:00',
-//   //   title: 'Dexter BD',
-//   //   description: '',
-//   //   allDay: false,
-//   //   free: true,
-//   //   color: '#3f51b5'
-//   // }, {
-//   //   id: 4,
-//   //   start: '2022-05-01T10:30',
-//   //   end: '2022-05-01T11:30',
-//   //   title: 'Stakeholder mtg.',
-//   //   description: '',
-//   //   allDay: false,
-//   //   free: false,
-//   //   color: '#f44437'
-//   // }];
-//   //
+  @ViewChild('popup', { static: false })
+  popup!: MbscPopup;
+  @ViewChild('colorPicker', { static: false })
+  colorPicker: any;
+  popupEventTitle: string | undefined;
+  popupEventDescription = '';
+  popupEventAllDay = true;
+  popupEventDates: any;
+  popupEventStatus = 'busy';
+  calendarSelectedDate: any = new Date();
+  switchLabel: any = 'All-day';
+  tempColor = '';
+  selectedColor = '';
+  colorAnchor: HTMLElement | undefined;
+  colors = ['#ffeb3c', '#ff9900', '#f44437', '#ea1e63', '#9c26b0', '#3f51b5', '', '#009788', '#4baf4f', '#7e5d4e'];
+
+
+  myEvents: MbscCalendarEvent[] = [{
+    id: 1,
+    start: '2022-04-08T13:00',
+    end: '2022-04-08T13:45',
+    title: 'Lunch @ Butcher\'s',
+    description: '',
+    allDay: false,
+    free: true,
+    color: '#009788'
+  }, {
+    id: 2,
+    start: '2022-04-30T15:00',
+    end: '2022-04-30T16:00',
+    title: 'General orientation',
+    description: '',
+    allDay: false,
+    free: false,
+    color: '#ff9900'
+  }, {
+    id: 3,
+    start: '2022-04-29T18:00',
+    end: '2022-04-29T22:00',
+    title: 'Dexter BD',
+    description: '',
+    allDay: false,
+    free: true,
+    color: '#3f51b5'
+  }, {
+    id: 4,
+    start: '2022-05-01T10:30',
+    end: '2022-05-01T11:30',
+    title: 'Stakeholder mtg.',
+    description: '',
+    allDay: false,
+    free: false,
+    color: '#f44437'
+  }];
+
 //   // tempEvent!: MbscCalendarEvent;
 //   // calendarOptions: MbscEventcalendarOptions = {
 //   //   clickToCreate: 'double',
@@ -290,9 +278,11 @@ export class ScheduleComponent implements OnInit {
 //   //   }
 //   // }
 //
-//   ngOnInit(): void {
 //
-//   }
-//
-//
-// }
+
+
+
+ngOnInit(): void {
+  }
+
+}
