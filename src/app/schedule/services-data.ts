66e3f2116f92a0a091ad5b24ddfa93e908cc4f20
ElinorPrenja <elinorprenja17@gmail.com>
2022-05-09 18:37:20 +0200

    shtimi i kohes ne popup

--- src/app/schedule/services-data.ts
+++ src/app/schedule/services-data.ts
@@ -1,26 +1,31 @@
 export const servicesData = [
   {
     name: 'Qethje',
+    timeMin: '45 min',
     time: 45,
     isSelected: false
   },
   {
     name: 'Stilim i mjekres',
+    timeMin: '10 min',
     time: 10,
     isSelected: false
   },
   {
     name: 'Trajtim fytyre',
+    timeMin: '5 min',
     time: 5,
     isSelected: false
   },
   {
     name: 'Maske fytyre',
+    timeMin: '5 min',
     time: 5,
     isSelected: false
   },
   {
     name: 'Stilim flokesh',
+    timeMin: '10 min',
     time: 10,
     isSelected: false
   }
